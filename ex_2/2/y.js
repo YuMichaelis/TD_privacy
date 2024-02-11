437d37b54795df5f4e50f16a6e5d487fc030410e
YuMichaelis <ambre.journot@gmail.com>
2024-02-11 20:39:16 +0100

    update

--- ex_2/2/y.js
+++ ex_2/2/y.js
@@ -3,10 +3,10 @@ const router = express.Router()
 
 
 router.get("/tstMe", (req, res) => { 
-    var r = /^[a-z]+$/;
+    var r = /([a-z]+)+$/;
 
     let match = r.test(req.params.id);
-    res.send(match.toString())
+    res.send(match)
     
 });
 
