8939c324fe459b34375aab45833cf970c9b41d5f
YuMichaelis <ambre.journot@gmail.com>
2024-02-11 21:32:44 +0100

    update

--- ex_2/4/aa.js
+++ ex_2/4/aa.js
@@ -30,14 +30,24 @@ router.get('/login',function(req, res){
     if(req.session.isAuthenticated()){
         res.redirect('http://example.com/'+followPath); //false positive
     }else{
-        res.redirect('/profile');
+        res.redirect('/');
     }
 }); 
 
-router.get('/goto',function(req, res){
-    let url = encodeURI(req.query.url); //vulnerability
-    res.redirect(url);
-}); 
+const validUrl = require('valid-url'); // Import the valid-url library or any other URL validation library
+
+router.get('/goto', function (req, res) {
+    const url = req.query.url;
+
+    // Validate the URL
+    if (validUrl.isUri(url)) {
+        // URL is valid, perform the redirect
+        res.redirect(url);
+    } else {
+        // URL is not valid, handle the error (e.g., redirect to a safe location)
+        res.status(400).send('Invalid URL');
+    }
+});
 
 
 module.exports = router
