7f0e3e90a729601c6fe0a263eeb214edcf72d992
YuMichaelis <ambre.journot@gmail.com>
2024-02-11 17:13:09 +0100

    update

--- ex_3/vulpy-ssl.py
+++ ex_3/vulpy-ssl.py
@@ -8,9 +8,11 @@ from mod_posts import mod_posts
 from mod_mfa import mod_mfa
 
 import libsession
+import os
+
 
 app = Flask('vulpy')
-app.config['SECRET_KEY'] = 'aaaaaaa'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'defaultSecretKey')
 
 app.register_blueprint(mod_hello, url_prefix='/hello')
 app.register_blueprint(mod_user, url_prefix='/user')
