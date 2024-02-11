190a3588453921c8df5f56cf98b8f5f28818d413
YuMichaelis <ambre.journot@gmail.com>
2024-02-11 11:25:29 +0100

    update

--- ex_2/4/koa.js
+++ ex_2/4/koa.js
@@ -4,16 +4,8 @@ const urlLib = require('url');
 const app = new Koa();
 
 app.use(async ctx => {
-    var url = ctx.query.target;
-    // Liste des domaines approuvés
-    const allowedDomains = ['http://example.com', 'https://yourdomain.com'];
-    const hostname = urlLib.parse(url).hostname;
-
-    if (allowedDomains.includes(`http://${hostname}`) || allowedDomains.includes(`https://${hostname}`)) {
-        ctx.redirect(url);
-    } else {
-        ctx.body = "Redirection non autorisée.";
-    }
+	var url = ctx.query.target;
+	ctx.redirect(url); 
 });
 
 app.listen(3000);
