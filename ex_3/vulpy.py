ce9d7b2614aff436409e9556e5ad5490b915b4d4
YuMichaelis <ambre.journot@gmail.com>
2024-02-11 22:56:19 +0100

    update

--- ex_3/vulpy.py
+++ ex_3/vulpy.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
+
 from pathlib import Path
 
+
 from flask import Flask, g, redirect, request
+
 
 import libsession
 from mod_api import mod_api
@@ -12,8 +15,13 @@ from mod_mfa import mod_mfa
 from mod_posts import mod_posts
 from mod_user import mod_user
 
+
+import os
+
+
 app = Flask('vulpy')
-app.config['SECRET_KEY'] = 'aaaaaaa'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'your_default_secret_key')
+
 
 app.register_blueprint(mod_hello, url_prefix='/hello')
 app.register_blueprint(mod_user, url_prefix='/user')
@@ -22,34 +30,42 @@ app.register_blueprint(mod_mfa, url_prefix='/mfa')
 app.register_blueprint(mod_csp, url_prefix='/csp')
 app.register_blueprint(mod_api, url_prefix='/api')
 
+
 csp_file = Path('csp.txt')
 csp = ''
 
+
 if csp_file.is_file():
-    with csp_file.open() as f:
-        for line in f.readlines():
-            if line.startswith('#'):
-                continue
-            line = line.replace('\n', '')
-            if line:
-                csp += line
+   with csp_file.open() as f:
+       for line in f.readlines():
+           if line.startswith('#'):
+               continue
+           line = line.replace('\n', '')
+           if line:
+               csp += line
 if csp:
-    print('CSP:', csp)
+   print('CSP:', csp)
+
+
 
 
 @app.route('/')
 def do_home():
-    return redirect('/posts')
+   return redirect('/posts')
+
 
 @app.before_request
 def before_request():
-    g.session = libsession.load(request)
+   g.session = libsession.load(request)
+
 
 @app.after_request
 def add_csp_headers(response):
-    if csp:
-        response.headers['Content-Security-Policy'] = csp
-    return response
+   if csp:
+       response.headers['Content-Security-Policy'] = csp
+   return response
+
+
 
 
 app.run(debug=True, host='127.0.1.1', port=5000, extra_files='csp.txt')
