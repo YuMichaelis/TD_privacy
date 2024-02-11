fd8410a4bb28aea43427de93d55a95a690a50b14
YuMichaelis <ambre.journot@gmail.com>
2024-02-11 10:33:49 +0100

    update

--- ex_2/3/express.js
+++ ex_2/3/express.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
+function escapeHTML(str) {
+    return str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#039;');
+}
+
 router.get('/greeting', (req, res) => {
     const { name }  = req.query;
-    res.send('<h1> Hello :'+ name +"</h1>")
+    res.send('<h1>Hello: ' + escapeHTML(name) + '</h1>');
 })
 
 router.get('/greet-template', (req,res) => {
