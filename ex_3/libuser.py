17273ff29f5152dbd60b7056930e5055c9953b70
YuMichaelis <ambre.journot@gmail.com>
2024-02-11 22:49:10 +0100

    update

--- ex_3/libuser.py
+++ ex_3/libuser.py
@@ -2,60 +2,74 @@ import sqlite3
 import libuser
 
 
+
+
 def login(username, password):
 
-    conn = sqlite3.connect('db_users.sqlite')
-    conn.set_trace_callback(print)
-    conn.row_factory = sqlite3.Row
-    c = conn.cursor()
 
-    user = c.execute("SELECT * FROM users WHERE username = '{}' and password = '{}'".format(username, password)).fetchone()
+   conn = sqlite3.connect('db_users.sqlite')
+   conn.set_trace_callback(print)
+   conn.row_factory = sqlite3.Row
+   c = conn.cursor()
 
-    if user:
-        return user['username']
-    else:
-        return False
+
+   user = c.execute("SELECT * FROM users WHERE username = ? and password = ?", (username, password)).fetchone()
+
+
+   if user:
+       return user['username']
+   else:
+       return False
+
+
 
 
 def create(username, password):
 
-    conn = sqlite3.connect('db_users.sqlite')
-    c = conn.cursor()
 
-    c.execute("INSERT INTO users (username, password, failures, mfa_enabled, mfa_secret) VALUES ('%s', '%s', '%d', '%d', '%s')" %(username, password, 0, 0, ''))
+   conn = sqlite3.connect('db_users.sqlite')
+   c = conn.cursor()
 
-    conn.commit()
-    conn.close()
+
+   c.execute("INSERT INTO users (username, password, failures, mfa_enabled, mfa_secret) VALUES (?, ?, ?, ?, ?)", (username, password, 0, 0, ''))
+
+
+
+
+   conn.commit()
+   conn.close()
+
+
 
 
 def userlist():
 
-    conn = sqlite3.connect('db_users.sqlite')
-    conn.set_trace_callback(print)
-    conn.row_factory = sqlite3.Row
-    c = conn.cursor()
 
-    users = c.execute("SELECT * FROM users").fetchall()
+   conn = sqlite3.connect('db_users.sqlite')
+   conn.set_trace_callback(print)
+   conn.row_factory = sqlite3.Row
+   c = conn.cursor()
 
-    if not users:
-        return []
-    else:
-        return [ user['username'] for user in users ]
+
+   users = c.execute("SELECT * FROM users").fetchall()
+
+
+   if not users:
+       return []
+   else:
+       return [ user['username'] for user in users ]
+
+
 
 
 def password_change(username, password):
 
-    conn = sqlite3.connect('db_users.sqlite')
-    conn.set_trace_callback(print)
-    conn.row_factory = sqlite3.Row
-    c = conn.cursor()
 
-    c.execute("UPDATE users SET password = '{}' WHERE username = '{}'".format(password, username))
-    conn.commit()
-
-    return True
+   conn = sqlite3.connect('db_users.sqlite')
+   conn.set_trace_callback(print)
+   conn.row_factory = sqlite3.Row
+   c = conn.cursor()
 
 
-def password_complexity(password):
-    return True
-
+   c.execute("UPDATE users SET password = ? WHERE username = ?", (password, username))
+   conn.commit()
