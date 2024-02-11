ae2725f20b5b9caa31a8c22017d11e8c77a8780b
YuMichaelis <ambre.journot@gmail.com>
2024-02-11 11:01:22 +0100

    update

--- ex_2/3/express.js
+++ ex_2/3/express.js
@@ -1,14 +1,23 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const sanitizeHtml = require('sanitize-html');
+const router = express.Router();
 
 router.get('/greeting', (req, res) => {
-    const { name }  = req.query;
-    res.send('<h1> Hello :'+ name +"</h1>")
-})
+    const { name } = req.query;
+    const sanitized = sanitizeHtml(name, {
+        allowedTags: [],
+        allowedAttributes: {} 
+    });
+    res.send(`<h1>Hello: ${sanitized}</h1>`);
+});
 
-router.get('/greet-template', (req,res) => {
-    name = req.query.name
-    res.render('index', { user_name: name});
-})
+router.get('/greet-template', (req, res) => {
+    const name = req.query.name;
+    const sanitized = sanitizeHtml(name, {
+        allowedTags: [],
+        allowedAttributes: {}
+    });
+    res.render('index', { user_name: sanitized });
+});
 
-module.exports = router
+module.exports = router;
