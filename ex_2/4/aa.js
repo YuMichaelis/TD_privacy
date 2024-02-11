95fdcc83cf8b3648c423b23f2a677c5672d995ad
YuMichaelis <ambre.journot@gmail.com>
2024-02-11 21:27:00 +0100

    update

--- ex_2/4/aa.js
+++ ex_2/4/aa.js
@@ -1,4 +1,26 @@
 
+// console.log('WIP')
+// const express = require('express');
+// const router = express.Router()
+
+// router.get('/login',function(req, res){
+//     let followPath = req.query.path;
+//     if(req.session.isAuthenticated()){
+//         res.redirect('http://example.com/'+followPath); //false positive
+//     }else{
+//         res.redirect('/');
+//     }
+// }); 
+
+// router.get('/goto',function(req, res){
+//     let url = encodeURI(req.query.url); //vulnerability
+//     res.redirect(url);
+// }); 
+
+
+// module.exports = router
+
+
 console.log('WIP')
 const express = require('express');
 const router = express.Router()
@@ -8,26 +30,13 @@ router.get('/login',function(req, res){
     if(req.session.isAuthenticated()){
         res.redirect('http://example.com/'+followPath); //false positive
     }else{
-        res.redirect('/');
+        res.redirect('/profile');
     }
 }); 
 
-router.get('/goto', function(req, res) {
-    let url = req.query.url; // Obtenez l'URL à partir de la requête
-    const allowedDomains = ['https://example.com', 'https://trusteddomain.com']; // Liste des domaines autorisés
-
-    try {
-        const parsedUrl = new URL(url);
-        const isValidDomain = allowedDomains.some(domain => parsedUrl.hostname === new URL(domain).hostname);
-        
-        if (isValidDomain) {
-            res.redirect(url);
-        } else {
-            res.status(400).send('Redirection non autorisée.'); // Ou rediriger vers une page d'erreur
-        }
-    } catch (error) {
-        res.status(400).send('URL invalide.');
-    }
+router.get('/goto',function(req, res){
+    let url = encodeURI(req.query.url); //vulnerability
+    res.redirect(url);
 }); 
 
 
